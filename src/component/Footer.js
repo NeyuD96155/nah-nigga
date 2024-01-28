5a1b765d20990dd7eae92c194b3021f4b1f6dfc8
https://github.com/NeyuD96155 <phuoc10a12az@gmail.com>
2024-01-28 10:04:52 +0700

    footer link

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link
 import '../style/main.css';
+
 const Footer = () => {
   return (
     <footer className="app-footer">
@@ -7,16 +9,17 @@ const Footer = () => {
         <section className="footer-section">
           <h4>About</h4>
           <p>Learn more about My Application and our team.</p>
-          <a href="/about">About Us</a>
+          <Link to="/about">About Us</Link> {/* Use Link component for internal navigation */}
         </section>
         <section className="footer-section">
           <h4>Contact</h4>
           <p>Have questions? Get in touch with us.</p>
-          <a href="/contact">Contact Us</a>
+          <Link to="/contact">Contact Us</Link> {/* Use Link component for internal navigation */}
         </section>
         <section className="footer-section">
           <h4>Follow Us</h4>
           <div className="social-links">
+            {/* External links can remain as <a> tags */}
             <a href="https://www.facebook.com/profile.php?id=100087274279487">Facebook</a>
             <a href="https://twitter.com/i4dopamint">Twitter</a>
             <a href="https://www.instagram.com/yekwolah/">Instagram</a>
@@ -25,7 +28,7 @@ const Footer = () => {
         <section className="footer-section">
           <h4>Contributor</h4>
           <p>Subscribe to our newsletter for the latest updates.</p>
-          
+          {/* Newsletter subscription functionality can be added here */}
         </section>
       </div>
       <div className="footer-bottom">
